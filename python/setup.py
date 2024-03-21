3bf1e98a714400ef5062e2349462328f5dcde80f
carlo <carlo@aime.info>
2024-03-21 18:26:49 +0100

    version update

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='AIME API Client Interface',
-      version='0.5.1',
+      version='0.6.0',
       author='AIME',
       author_email='carlo@aime.info',
       packages=['aime_api_client_interface'],
