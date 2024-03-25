a06e3d1a51d6031de44e48dcc6758ba94e624ae1
carlo <carlo@aime.info>
2024-03-25 18:32:50 +0100

    Added header with license and copyright to all files

--- python/aime_api_client_interface/__init__.py
+++ python/aime_api_client_interface/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (c) AIME GmbH and affiliates. Find more info at https://www.aime.info/api
+#
+# This software may be used and distributed according to the terms of the MIT LICENSE
+
 from .model_api import ModelAPI, do_api_request, do_api_request_async
